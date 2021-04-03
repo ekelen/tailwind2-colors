51b482dbd82995d67e870e63532d3643085ec750
Elizabeth Kelen <erskelen@gmail.com>
2021-04-03 15:26:13 -0400

    add basic labels

--- src/colorCreator.js
+++ src/colorCreator.js
@@ -2,27 +2,50 @@ const convert = require("color-convert");
 const colors = require("./colors").default;
 
 function colorCreator(baseColorName, value) {
-  const hslStr = colors[baseColorName][value];
+  this.hslValueArray = colors[baseColorName][value].split(",").map((v) => +v);
   this.baseColorName = baseColorName;
   this.value = value;
 
   this.tailwindName = `${baseColorName}-${value}`;
-  const [h, s, l] = hslStr.split(",").map((v) => +v);
+
+  const [h, s, l] = this.hslValueArray;
   this.h = h;
   this.s = s;
   this.l = l;
 }
 
+colorCreator.prototype.mapToHslArgs = function (
+  h = this.h,
+  s = this.s,
+  l = this.l
+) {
+  return [h, s + "%", l + "%"];
+};
+
 colorCreator.prototype.hex = function () {
-  return `#${convert.hsl.hex(this.h, this.s, this.l)}`;
+  return `#${convert.hsl.hex(this.hslValueArray)}`;
 };
 
 colorCreator.prototype.hsl = function () {
-  return `hsl(${this.h}, ${this.s}%, ${this.l}%)`;
+  return `hsl(${this.mapToHslArgs()})`;
 };
 
 colorCreator.prototype.rgb = function () {
-  return `rgb(${convert.hsl.rgb(this.h, this.s, this.l)})`;
+  return `rgb(${convert.hsl.rgb(this.hslValueArray)})`;
+};
+
+colorCreator.prototype.muchContrast = function () {
+  const lightness = this.l <= 50 ? 88 : 12;
+  return `hsl(${this.mapToHslArgs(this.h, this.s, lightness)})`;
+};
+
+colorCreator.prototype.allFormats = function () {
+  return {
+    hex: this.hex(),
+    rgb: this.rgb(),
+    hsl: this.hsl(),
+    tailwindName: this.tailwindName,
+  };
 };
 
 globalThis.colors = colors;
--- static/index.js
+++ static/index.js
@@ -11,14 +11,20 @@ const createColorRows = (containerEl, colors, colorCreator) => {
     const blockClone = blockTemplate.content.cloneNode(true);
     const block = blockClone.querySelector(".colorBlock");
     const color = new colorCreator(baseColorName, colorValue);
-    block.style.backgroundColor = color.hex();
+    const moreContrast = color.muchContrast();
+
+    const formats = color.allFormats();
+    block.style.backgroundColor = formats.hsl;
+    block.style.color = moreContrast;
+    block
+      .querySelectorAll(".label")
+      .forEach((el) => (el.textContent = formats[el.dataset.format]));
     rowEl.appendChild(block);
   };
 
   const createRow = (baseColorName) => {
     const rowClone = rowTemplate.content.cloneNode(true);
     const row = rowClone.querySelector(".colorRow");
-    row.id = baseColorName;
     const colorValues = Object.keys(colors[baseColorName]);
     colorValues.forEach((value) => {
       createBlock(baseColorName, row, value);
@@ -30,11 +36,30 @@ const createColorRows = (containerEl, colors, colorCreator) => {
   baseColors.forEach(createRow);
 };
 
+const clickOnCopy = function (stealthInput) {
+  document.querySelectorAll(".label").forEach((node) => {
+    node.addEventListener("click", (e) => {
+      e.preventDefault();
+      stealthInput.value = e.target.textContent;
+      stealthInput.focus({ preventScroll: true });
+      stealthInput.select();
+      document.execCommand("copy");
+      e.target.textContent = "Copied!";
+      window.setTimeout(
+        () => (e.target.textContent = stealthInput.value),
+        2000
+      );
+    });
+  });
+};
+
 window.addEventListener("DOMContentLoaded", () => {
   const containerEl = document.querySelector("#container");
+  const stealthInput = document.querySelector("#stealthInput");
   const { colors, colorCreator } = globalThis;
   if (!colors || !colorCreator) {
     containerEl.textContent = "Hmm, we can't find the right JS files.";
   }
   createColorRows(containerEl, colors, colorCreator);
+  clickOnCopy(stealthInput);
 });
